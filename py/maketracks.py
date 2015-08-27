f817c83330320c911c069a4063f87f23103b365b
Jonatan Selsing <jselsing@dark-cosmology.dk>
2015-08-27 12:18:23 -0700

    Implemented reddening

--- py/maketracks.py
+++ py/maketracks.py
@@ -10,9 +10,13 @@ rc_file('/Users/jselsing/Pythonlibs/plotting/matplotlibstyle.rc')
 import matplotlib.pylab as pl
 import seaborn as sns; sns.set_style('ticks')
 # cmap = sns.cubehelix_palette(n_colors=6, start=0.0, rot=1.5, gamma=1.0, hue=1.0, light=0.85, dark=0.15, reverse=True, as_cmap=False)
-cmap = sns.color_palette("cubehelix", 3)
+cmap = sns.color_palette("cubehelix", 7)
 
 from util import synth_mag
+
+from specutils.extinction import reddening
+from astropy import units as u
+
 
 
 
@@ -22,40 +26,54 @@ def main():
 	wl, flux, error = composite[:,0], composite[:,1], composite[:,2]
 	dz = np.arange(0, 5, 0.01)
 	bands = ['u', 'g', 'r', 'i', 'z', 'Z2', 'Y', 'J', 'H', 'K']
-	mags = collections.defaultdict(list)
-
-	#Loop through redshifts
-	for i in dz:
-		wl_z = wl * (1+i)
-		flux_z = flux / (1+i)
-		error_z = error / (1+i)
-
-		#Loop through bands
-		for k in bands:
-			mag = synth_mag(band=k, datapath='../data/filter_curves/', wave=wl_z, flux=flux_z)
-			mags[k].append(mag)
 
 
-	#Calculate colors
-	JK = np.array(mags['J']) - np.array(mags['K'])
-	gJ =  np.array(mags['g']) - np.array(mags['J'])
-
-
-	#Printing
-	print('J - K:', JK)
-	print('J:', np.array(mags['J']))
-	print('g - J:', gJ)
-
-	#Make the plot
+	AV = -1 * np.array([0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 2.0])
+	# EBV = AV
+	# colors = 
 	fig, ax = pl.subplots()
-	sca = ax.scatter(JK, gJ, c=dz)
-	ax.set_xlim((-1, 2))
-	ax.set_ylim((-0.5, 2.0))
+
+	for av in AV:
+		# print(ebv)
+		mags = collections.defaultdict(list)
+		#Loop through redshifts
+		for i in dz:
+			wl_z = wl * (1+i)
+			wl_z = wl_z[np.where(wl_z < 60000)]
+			flux_z = (flux / (1+i))[np.where(wl_z < 60000)]
+			error_z = (error / (1+i))[np.where(wl_z < 60000)]
+			flux_z =  flux_z * reddening(wl_z* u.angstrom, a_v=av, r_v=3.1, model='fm07')
+			error_z = error_z * reddening(wl_z* u.angstrom, a_v=av, r_v=3.1, model='fm07')
+
+			#Loop through bands
+			for k in bands:
+				mag = synth_mag(band=k, datapath='../data/filter_curves/', wave=wl_z, flux=flux_z)
+				mags[k].append(mag)
+
+
+		#Calculate colors
+		JK = np.array(mags['J']) - np.array(mags['K'])
+		gJ =  np.array(mags['g']) - np.array(mags['J'])
+		gr = np.array(mags['g']) - np.array(mags['r'])
+
+
+
+		#Printing
+		# print('J - K:', JK)
+		# print('J:', np.array(mags['J']))
+		# print('g - J:', gJ)
+
+		#Make the plot
+		sca = ax.scatter(JK, gr, c=dz)
+		ax.plot(JK, gr, label= r'Track for: A$_{V}$ = '+str(av))	
+	ax.set_xlim((-1.0, 1.0))
+	ax.set_ylim((-1.0, 2.0))
 	cbar = fig.colorbar(sca, ax=ax)
 	ax.set_xlabel('J - K')
-	ax.set_ylabel('g - J')
+	ax.set_ylabel('g - r')
 	cbar.set_label('z')
-	pl.savefig('../figs/color_track.pdf')
+	pl.legend()
+	pl.savefig('../figs/color_track_JKgr.pdf')
 	pl.show()
 
 
